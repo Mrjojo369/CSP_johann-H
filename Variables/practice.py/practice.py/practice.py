33353a6d26c03552507c246782f696aad9eaa104
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-01-29 09:52:46 -0700

    python practice

---
+++ Variables/practice.py/practice.py/practice.py
@@ -0,0 +1,21 @@
+#johann Hines, Variables practice python. 
+name="johann Hines"
+number1_10= "8"
+number100_1,000="700"
+Breakfast="panckakes"
+Favorite_color="blue"
+School_name="Utah academy of sciences"
+Current_year="2025"
+My_eye_color="blue"
+My_age="14"
+Favorite_school_subject="biology"
+print(name)
+print(number1_10)
+print(number100_1,000)
+print(breakfast)
+print(favorite_color)
+print(School_name)
+print(Current_year)
+print(My_eye_color)
+print(My_age)
+Print(Favorite_school_subject)
