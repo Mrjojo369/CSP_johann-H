0f12aa840dc8923c1b6574644e58a7ddbd7cce70
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-01-30 09:52:48 -0700

    C NOTES

--- Variables/practice.py/notes.py
+++ Variables/practice.py/notes.py
@@ -4,3 +4,7 @@ name = input("please tell me your name: ")
 
 print(name, ", welcome to my program.")
 print("it doesnt do anything yet")
+
+int main(void){  
+ printf("hello world"); 
+ return 0;
--- template.c
+++ template.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+char name[] ="johann";
+int num = 42;
+float pi = 3.14;
+
 int main(void){
-    printf("hello world;")
-    return 0;"
+    printf("hello%s,my age is%d and my favorite number is %f\n\n", name);
+    printf("%d\n", num);
+    printf("%f\n",pi);
+    return 0;
 }
