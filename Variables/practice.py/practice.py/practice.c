2897a408a5d4d3ddb075d018c574ba0088f95874
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-01-31 09:39:46 -0700

    c.practice

---
+++ Variables/practice.py/practice.py/practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char name[]="johann";
+int small_number= 7;
+int big_number= 700;
+char breakfast[]="panckakes";
+char favorite_color[]="blue";
+char school_name[]="ucas";
+int current_year= 2025;
+char eye_color[]="blue";
+int age= 14;
+char favorite_subject[]="world civilizations";
+
+int main (void){
+    printf("%s\n",name);
+    printf("%d\n",small_number);
+    printf("%d\n",big_number);
+    printf("%s\n",breakfast);
+    printf("%s\n",favorite_color);
+    printf("%s\n",school_name);
+    printf("%d\n",current_year);
+    printf("%s\n",eye_color);
+    printf("%d\n",age);
+    printf("%s\n",favorite_subject);
+    return 0;
+}
