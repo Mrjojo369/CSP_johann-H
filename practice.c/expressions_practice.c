cd6650a0305ec3731821d974421aa16475e5b57e
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-05 09:51:59 -0700

    expressions_practice.py

---
+++ practice.c/expressions_practice.c
@@ -0,0 +1,16 @@
+//johann hines, expressions practice.c 
+#include <stdio.h>
+#include <math.h>
+
+int a1= (7-24/8*4+6);
+int a2= (18/3-7+2*5);
+int a3= ()
+
+
+
+int main(void){
+    printf("%d\n", a1);
+    printf("%d\n", a2);
+    printf("%d\n", a3)
+    return 0;
+}
