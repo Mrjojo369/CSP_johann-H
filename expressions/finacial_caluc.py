aaf7d215ff7dda5c607cdf2e1618382a90ede570
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-06 09:48:54 -0700

    set up expressions calc.

--- expressions/finacial_caluc.py
+++ expressions/finacial_caluc.py
@@ -1,1 +1,39 @@
 # Johann hines, Financial calculator python. 
+
+# write a print statement telling the user what the program is (budget calculator)
+
+income=input ("what is your monthly income?")
+income = float (input("What is your monthly income\n"))
+
+# ask for monthly rent amount (user input)
+
+
+# ask for monthly utilities (user input)
+
+# ask for monthly transportation amount (user input)
+
+# ask for groceries amount (user input)
+
+# calculate savings as 10% of income 
+
+# calculate spending money income - (rent + income + utilities + transportation + groceries + savings) (variable)
+
+#calcualte the percent of rent (rent/income) (variable)
+
+#calcualte the percent of utiliuties (rent/income) (variable)
+
+#calcualte the percent of transportation (transportatipon/income) (variable)
+
+#calcualte the percent of groceries (groceries/income) (variable)
+
+# calculate The percent of spending money (spending/income) (vairblae)
+
+#tell user catagory spending amount and percent for monthly rent ("you spend $XX.XX on rent and that is xx% of your income")
+
+#tell user catagory spending amount and percent for utilites ("you spend $XX.XX on utilites and that is xx% of your income")
+
+#tell user catagory spending amount and percent for Transportation  ("you spend $XX.XX on transportation and that is xx% of your income")
+
+#tell user catagory spending amount and percent for groceries ("you spend $XX.XX on groceries and that is xx% of your income")
+
+#tell user catagory spending amount and percent for spening money("you spend $XX.XX on spending money and that is xx% of your income")
