edc4cd0a71b5141ae01136d929a8587e6ddb18ce
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-10 09:52:22 -0700

    string_notes

---
+++ strings/notes.py
@@ -0,0 +1,19 @@
+# Johann Hines, Strings notes.py
+
+# Words in programing(Data Type)"" ''
+ 
+#print("hello world")
+#name = input ("what is your name?(first name only please)\n").strip().capitalize()
+
+number = input ("give me a number pretty please\n").strip()
+num = 5*2
+#print(f"Hello {name} Welcome to my program")
+
+#print(number + 1)
+#print(num)
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+print sentence.find("fox")
+
+
