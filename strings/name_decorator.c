5ccc3d8250bd19f8fd9af1600269703a06499806
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-12 09:53:18 -0700

    name dec.c

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -3,7 +3,7 @@
 #include <string.h>
 /// @brief 
 
-char name[20] = "what is your name?";
+char name[20];
 char dec1[20] = "<<";
 char dec2[20] = ">>";
 
@@ -11,7 +11,7 @@ int main(void){
  printf("please tell me your full name\n");
 scanf("%s", name);
 strcat(dec1, name);
-strcat(dec2, name);
-printf("This is your name bedazled", );
+strcat(dec1, dec2 );
+printf("This is your name bedazled %s\n",dec1);
     return 0;
 }
