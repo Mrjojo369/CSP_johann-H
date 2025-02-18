f08dbaf0209f9b367be545f7e56c931ddad124d4
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-18 09:17:52 -0700

    fixed financial calc.py

---
+++ functions/notes.py
@@ -0,0 +1,11 @@
+# Johann hines, Functions notes in python 
+
+#function is ana action stored in a key word 
+
+def add():
+    numOne = int(("Give me a numberp:\n"))
+    numTwo = int(("Give me a numberp:\n"))
+    
+    print(numOne+numTwo)
+
+    add()
