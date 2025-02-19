2e1b4dd0d9c703473f770e1f8fb1fd842d6da81f
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-19 09:49:20 -0700

    fixed first program.c

--- functions/notes.c
+++ functions/notes.c
@@ -1,13 +1,13 @@
-//johann hines, expressions notes
+//johann hines, function notes.c
 #include <stdio.h>
 
 void add(int numOne, int numTwo){
     printf("%d\n", numOne + numTwo);
 }
-char input(char type[20]){
- char answer[50];
- printf("please give me a %s;\n",type);
- scanf("%s", answer);
+//const char* input(char type[50], int length){
+ //char answer[50];
+ //printf("please give me a %s;\n",type);
+ //getStr(answer, sizeof(answer)-1);  DO NOT RETURN STRINGS IN C!!!
  return answer;
 }
 
@@ -18,9 +18,9 @@ int main(void){
     //add(5,10);
    // add(1,99);
    // add(5,7);
-    input("name");
-    input("verb");
-    input("place");
-    printf("%s was %s untill they somehow reached %s", input("name"), input("verb"), input("place"));
+    word("name");
+    word("verb");
+    word("place");
+    printf("%s was %s untill they somehow reached %s", word("name", 50), word("verb", 50), word("place", 50));
     return 0; 
 }
