4ae4e8fb07e8145623da94718751cf2002a71810
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-20 09:46:12 -0700

    silly_sentences_py

---
+++ strings/silly_sentences.py
@@ -0,0 +1,9 @@
+#johann Hines, silly sentences.py 
+
+name= input ("Please tell me your name")
+
+action= input ("tell me a funny action")
+
+drink= input ("what is your favorite drink?")
+
+print(f"{name} was doing {action} when a bottle full of {drink} fell from the sky")
