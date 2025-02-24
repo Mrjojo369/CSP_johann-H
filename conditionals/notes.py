cf914e48c7fecdbd0d93aa7bff4875b9f05c3cad
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-24 09:32:17 -0700

    financial calc

---
+++ conditionals/notes.py
@@ -0,0 +1,25 @@
+# Johann Hines, Conditionals notes 
+
+#print("Hello wlecome to my program that will sort you into a catergory")
+
+#name = input("What is your name:\n")
+
+#if name == "Johann":
+ #print("oh your the tacher? ... nevermind")
+#else:
+ #print("You are a student welcome to class")
+
+# Two equal signs(==) check to see if the content is the same between two items. as well as checking if data type is the same
+
+num = 5
+
+if num == 1:
+    print("You have asked for a item")
+elif num <= 3:
+       print("You have asked for a couple of an item")
+elif num <= 5:
+      print("you have asked for a few of the item or if your from the south you asked for a couple")
+else:
+ print("You have asked for some of the item")
+
+#conditionals start at the top and work their way down. They only take the first conditon that is correct. 
