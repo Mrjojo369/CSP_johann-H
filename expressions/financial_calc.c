3f3c707e1c100b95bbc9dd34e627e63ece4f3106
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-26 09:43:29 -0700

    expressions practice.c corrections

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -3,11 +3,9 @@
 char name [50];
 char income [50];
 char rent [50];
-int utilities [50];
-int groceries [50];
-int  transportation [50];
-int savings [50];
-int spending money [50];
+char utilities [50];
+char groceries [50];
+char  transportation [50];
 
 int main(void){
     printf("what is your name?\n");
@@ -23,18 +21,11 @@ int main(void){
       scanf("%s", groceries);
        printf("how much do you spend on transportation each month?\n");
        scanf("%s", transportation);
-     printf(rent/income)*100;
-     printf(utilities/income)*100;
-     printf(groceries/income)*100;
-     printf(transportation/income)*100;
-    
-savings = income * .2;
-    expenses = rent + utilites + groceries + transportation;
-    used = expenses - income - savings;
-    printf("you bring in %.2f\n", income);
-    printf("you spend about %.2f\n on expenses" , expenses);
-    printf("you will save approximatly %.2f\n", savings);
-    printf("you bring in %.2f\n", income);
-    printf("you will have about%.2f\n for spending money", spending money);
+     printf (rent/income)*100;
+     printf  (utilities/income)*100;
+     printf (groceries/income)*100;
+     printf  (transportation/income)*100;
+
+
     return 0;
 }
