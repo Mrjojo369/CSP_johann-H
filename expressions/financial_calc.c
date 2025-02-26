2dae6fe75fce9966b49461878e644f95e9eb69a7
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-26 09:51:33 -0700

    im so tired

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -1,26 +1,26 @@
 //johann hines, finacial calculator. 
 #include <stdio.h>
 char name [50];
-char income [50];
-char rent [50];
-char utilities [50];
-char groceries [50];
-char  transportation [50];
+float income [50];
+ float rent [50];
+float utilities [50];
+float groceries [50];
+float  transportation [50];
 
 int main(void){
     printf("what is your name?\n");
     scanf ("%s", name);
     printf( "hello, %s welcome to my financial calculator\n", name);
     printf("how much do you make per month?\n");
-    scanf("%s", income);
+    scanf("%f", income);
     printf("how much do you spend on rent each month?\n");
-    scanf("%s", rent);
+    scanf("%f", rent);
      printf("how much do you spend on utilities per month?\n");
-     scanf("%s", utilities);
+     scanf("%f", utilities);
       printf("how much do you spend on groceries per month?\n");
-      scanf("%s", groceries);
+      scanf("%f", groceries);
        printf("how much do you spend on transportation each month?\n");
-       scanf("%s", transportation);
+       scanf("%f", transportation);
      printf (rent/income)*100;
      printf  (utilities/income)*100;
      printf (groceries/income)*100;
