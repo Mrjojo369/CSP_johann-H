8558e3cb3fcbac829fac81632cb7cc60720f7dc9
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-02-28 09:43:27 -0700

    update of first programm for c and python

---
+++ functions/update hello world.py
@@ -0,0 +1,16 @@
+#johann Hines updtae first program
+name1 = "my glorius king james"
+name2 = "marlon funaki"
+name3 = "Lecinnabon james"
+name4 = "bart leo"
+name5 = "quandale dingle the 1st"
+#print({name1}, ",welcome to my first programm")
+
+def user(username):{
+    print(f"Hello {username}")
+}
+user (name1) 
+user (name2)
+user (name3)
+user (name4)
+user (name5)
--- functions/update_helloworld.c
+++ functions/update_helloworld.c
@@ -1,9 +1,18 @@
  //johann hines, update hello world
 #include <stdio.h>
-
-
+void names(char names[]){
+    printf("Hello %s\n", names );
+}
 int main(void){
-    printf("hello world");
-    word 
+    char name_1 [50] = ("johan gillian") ;
+    char name_2 [50] = ("dana white");
+    char name_3 [50] = ("lecinnibon james");
+    char name_4 [50] = ("quandale dingle the 3rd");
+    char name_5 [50] = ("juandaledingle");
+     names(name_1);
+      names(name_2);
+       names(name_3);
+        names(name_4);
+         names(name_5);
     return 0;
 }
