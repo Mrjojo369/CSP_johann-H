34cd208b53c6157c72d49858be88a0922b520b74
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-03-06 09:49:21 -0700

    did family names for python

--- loops/family_names.py
+++ loops/family_names.py
@@ -1,11 +1,11 @@
 #johann hines, family names for python 
 
-siblings =["Hello lebron james", "Hello quandale dingle", "Hello brianna", "Hello samira", "Hello brian", " Hello samantha", "Hello amanda"]
+siblings =["lebron james", "quandale dingle", "brianna", "samira", "brian", "samantha", "amanda"]
 
-siblings.pop(5)
-print(siblings)
-num = 6
-print(f"{num}.{siblings} hines")
-num += 1
-for num in range(2,10, 2):
-     print(num)
+
+for siblings in siblings:
+  print(f"Hello {siblings}")
+
+
+
+
