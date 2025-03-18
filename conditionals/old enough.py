21f69f3c49891e7b2d3a9176e01996a892be901a
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-03-18 09:27:34 -0600

    old enough.py

--- conditionals/old enough.py
+++ conditionals/old enough.py
@@ -1,6 +1,12 @@
-age = 15
+age = 17
 
-if "a" in name or "e" in name or "i" in name or "o" in name or "u" in name:
-    print("your name has a vowel")
+if age >= 18:
+    print("you are old enough to vote")
+elif age >= 16:
+    print("You are old enough to drive. but not quite old enough to vote")
+elif age == 15:
+    print("You can get your learners permit")
+elif age >= 7:
+    print("You are old enough to go to school")
 else: 
-    print("your name doesnt have a vowel")
+    print("you are too young to do anything.")
