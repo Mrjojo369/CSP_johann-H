495ed061f60335f2cad0fe833555ded549b11ec6
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-03-19 09:52:09 -0600

    started final project

---
+++ final.py
@@ -0,0 +1,7 @@
+#Johann, Hines (the coolest)
+
+Greet =  input ("what is your name?\n")
+print (f"Hello, {Greet} welcome to our game, you will be playing tic tac toe against an ai.")
+
+pick_spot = input ("Pick a number 1, 2, 3, 4, 5, 6, 7, 8, or 9,")
+print (f"")
