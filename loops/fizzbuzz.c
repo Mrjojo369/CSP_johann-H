0f36837790c840d388ffb4869711cb311d0be384
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-03-21 00:10:46 -0600

    Update fizzbuzz.c

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,1 +1,15 @@
+ //johann hines, template
+#include <stdio.h>
 
+
+int main(void){
+  if (x % 5 == 0 && x % 3 == 0){
+    printf("fizzbuzz");
+  }else if (x % 5 == 0){
+    printf(buss");
+  }elseif(x % 3 == 0){
+    print("fizz");
+  }else{
+    printf("%d", x)
+    return 0;
+}
