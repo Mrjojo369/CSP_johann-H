324937caed653d0ad9a4ee090d91fd52fb10c402
Mrjojo369 <Johann.hines@ucas-edu.net>
2025-03-24 09:16:03 -0600

    code for ryan

--- final.py
+++ final.py
@@ -1,6 +1,40 @@
 #Johann, Hines (the coolest)
+grid = [[1,2,3],
+[4,5,6],
+[7,8,9]]
 
 Greet =  input ("what is your name?\n")
 print (f"Hello, {Greet} welcome to our game, you will be playing tic tac toe against an ai.")
-
-pick_spot = input ("Pick a number 1 through 9")
+game_finished = False 
+taken = []
+while game_finished == False:
+        spot = input("pick a number for your spot 1-9\n !!!SPELL OUT DONT TYPE A NUMBER!!!\n").strip().capitalize()
+    
+        if spot != taken:
+            taken.append(spot)
+            if spot == "ONE":
+                grid[0][0]="X"
+            elif spot == "TWO":
+                grid[0][1]  ="X"
+            elif spot == "THREE":
+                grid[0][2]="X"
+            elif spot == "FOUR":
+                grid[1][0]="X"
+            elif spot == "FIVE": 
+                grid[1][1]="X"
+            elif spot == "SIX":
+                grid[1][2] ="X"
+            elif spot == "SEVEN":
+                grid[2][0] ="X"
+            elif spot == "EIGHT":
+                grid[2][1] ="X"
+            elif spot == "NINE":
+                grid[2][2] ="X"
+        else :
+            print("Dummy that number is already chosen or not in the options")
+            continue
+for grid in grid:
+    print(f"{grid[0]}|{grid[1]}|{grid[2]}\n --+---+--")
+    
+print(taken)
+   
